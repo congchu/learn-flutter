4e342206efab0f85cf4db93c668f4610dcad1d5d
congchu <cookie00421@gmail.com>
2022-02-01 18:55:45 +0900

    [refactor] themeData 다른 파일(style.dart)로 빼서 import

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,8 @@
 import 'package:flutter/material.dart';
+import './style.dart' as style;
 
 void main() {
-  runApp(MaterialApp(
-      theme: ThemeData(
-          appBarTheme: AppBarTheme(
-              color: Colors.white,
-              elevation: 1, // 그림자 크기
-              titleTextStyle: TextStyle(color: Colors.black87, fontSize: 25),
-              actionsIconTheme: IconThemeData(color: Colors.black87)),
-          iconTheme: IconThemeData(color: Colors.pinkAccent),
-          textTheme: TextTheme(
-            headline6: TextStyle(color: Colors.blue), // AppBar()
-            bodyText1: TextStyle(color: Colors.green), // 다른 모든 위젯
-            bodyText2: TextStyle(color: Colors.red), // Text()
-            button: TextStyle(color: Colors.blue), // TextButton()
-            subtitle1: TextStyle(color: Colors.blue), // ListTile()
-          )),
-      home: const MyApp()));
+  runApp(MaterialApp(theme: style.theme, home: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/style.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// TIP: 변수를 다른 파일에서 쓸 일이 없으면 변수명 앞에 _ 붙이기
+
+var theme = ThemeData(
+    appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 1, // 그림자 크기
+        titleTextStyle: TextStyle(color: Colors.black87, fontSize: 25),
+        actionsIconTheme: IconThemeData(color: Colors.black87)),
+    iconTheme: IconThemeData(color: Colors.pinkAccent),
+    textTheme: TextTheme(
+      headline6: TextStyle(color: Colors.blue), // AppBar()
+      bodyText1: TextStyle(color: Colors.green), // 다른 모든 위젯
+      bodyText2: TextStyle(color: Colors.red), // Text()
+      button: TextStyle(color: Colors.blue), // TextButton()
+      subtitle1: TextStyle(color: Colors.blue), // ListTile()
+    ));
